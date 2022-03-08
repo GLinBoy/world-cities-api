eb64067e65c355b2a3b096506d555ad9539bca0e
GLinBoy <hojjatabedie@gmail.com>
2022-03-08 13:31:01 +0330

    📦 NEW: Added City entity

---
+++ src/main/kotlin/com/glinboy/cities/entity/City.kt
@@ -0,0 +1,20 @@
+package com.glinboy.cities.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class City(
+    @Id
+    var id: Long = -1,
+    var city: String = "",
+    var cityAscii: String = "",
+    var lat: Double = 0.0,
+    var lng: Double = 0.0,
+    var country: String = "",
+    var iso2: String = "",
+    var iso3: String = "",
+    var adminName: String = "",
+    var capital: String = "",
+    var population: Long = -1
+)
