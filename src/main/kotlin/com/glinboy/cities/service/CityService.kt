00ea199c9f03819df12a089be44ef84968e65250
GLinBoy <hojjatabedie@gmail.com>
2022-03-08 13:40:38 +0330

    📦 NEW: Added City service

---
+++ src/main/kotlin/com/glinboy/cities/service/CityService.kt
@@ -0,0 +1,8 @@
+package com.glinboy.cities.service
+
+import com.glinboy.cities.entity.City
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface CityService {
+}
