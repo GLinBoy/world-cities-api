5f804f1b26b95373479b1c7cf3f2eca77bea0c15
GLinBoy <hojjatabedie@gmail.com>
2022-03-08 13:41:13 +0330

    📦 NEW: Added Country service

---
+++ src/main/kotlin/com/glinboy/cities/service/CountryService.kt
@@ -0,0 +1,9 @@
+package com.glinboy.cities.service
+
+import com.glinboy.cities.entity.City
+import com.glinboy.cities.projection.Country
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface CountryService {
+}
