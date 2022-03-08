cd32ebe7964071dca4e2703b9ad800ffe53ed434
GLinBoy <hojjatabedie@gmail.com>
2022-03-08 13:35:12 +0330

    📦 NEW: Added find country query

--- src/main/kotlin/com/glinboy/cities/respository/CityRepository.kt
+++ src/main/kotlin/com/glinboy/cities/respository/CityRepository.kt
@@ -10,4 +10,8 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface CityRepository : JpaRepository<City, Long> {
+    @Query(value = "SELECT country, iso2, iso3 from CITY GROUP BY country, iso2, iso3",
+        countQuery = "SELECT COUNT(*) FROM (SELECT country, iso2, iso3 from CITY GROUP BY country, iso2, iso3)",
+        nativeQuery = true)
+    fun findDistinctCountry(pageable: Pageable): Page<Country>
 }
