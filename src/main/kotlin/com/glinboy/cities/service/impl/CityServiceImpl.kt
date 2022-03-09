b62d6c53f7320b8c408445cebc4a36a705344bbc
GLinBoy <hojjatabedie@gmail.com>
2022-03-09 12:16:53 +0330

    📦 NEW: Add City service impl

---
+++ src/main/kotlin/com/glinboy/cities/service/impl/CityServiceImpl.kt
@@ -0,0 +1,12 @@
+package com.glinboy.cities.service.impl
+
+import com.glinboy.cities.entity.City
+import com.glinboy.cities.respository.CityRepository
+import com.glinboy.cities.service.CityService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+
+@Service
+class CityServiceImpl(val cityRepository: CityRepository) : CityService {
+}
