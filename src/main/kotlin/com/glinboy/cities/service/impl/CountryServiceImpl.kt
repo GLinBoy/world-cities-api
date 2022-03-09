829a07cdc009029684f60274e3626311e0bc8ecd
GLinBoy <hojjatabedie@gmail.com>
2022-03-09 12:17:37 +0330

    📦 NEW: Added Country service impl

---
+++ src/main/kotlin/com/glinboy/cities/service/impl/CountryServiceImpl.kt
@@ -0,0 +1,13 @@
+package com.glinboy.cities.service.impl
+
+import com.glinboy.cities.entity.City
+import com.glinboy.cities.projection.Country
+import com.glinboy.cities.respository.CityRepository
+import com.glinboy.cities.service.CountryService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+
+@Service
+class CountryServiceImpl(val cityRepository: CityRepository): CountryService {
+}
