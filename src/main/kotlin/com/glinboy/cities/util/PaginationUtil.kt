9b25bc04bd0d9a4de15095b1549f78cf9e514871
GLinBoy <hojjatabedie@gmail.com>
2022-03-10 11:42:11 +0330

    📦 NEW: Added Pagination util

---
+++ src/main/kotlin/com/glinboy/cities/util/PaginationUtil.kt
@@ -0,0 +1,35 @@
+package com.glinboy.cities.util
+
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpHeaders
+import org.springframework.web.util.UriComponentsBuilder
+
+class PaginationUtil {
+    companion object {
+        fun <T> generateHeaders(page: Page<T>, baseUrl: String ): HttpHeaders {
+            val headers = HttpHeaders()
+            headers.set("X-Total-Count", page.totalElements.toString())
+            var link = ""
+            if (page.number + 1 < page.totalPages) {
+                link = "<" + generateUri(baseUrl, page.number + 1, page.size).toString() + ">; rel=\"next\","
+            }
+            if (page.number > 0) {
+                link += "<" + generateUri(baseUrl, page.number - 1, page.size).toString() + ">; rel=\"prev\","
+            }
+            var lastPage = 0
+            if (page.totalPages > 0) {
+                lastPage = page.totalPages - 1
+            }
+            link += "<" + generateUri(baseUrl, lastPage, page.size).toString() + ">; rel=\"last\","
+            link += "<" + generateUri(baseUrl, 0, page.size).toString() + ">; rel=\"first\""
+            headers.add(HttpHeaders.LINK, link)
+            return headers
+        }
+
+        private fun generateUri(baseUrl: String, page: Int, size: Int): String? = UriComponentsBuilder
+            .fromUriString(baseUrl)
+            .queryParam("page", page)
+            .queryParam("size", size)
+            .toUriString()
+    }
+}
