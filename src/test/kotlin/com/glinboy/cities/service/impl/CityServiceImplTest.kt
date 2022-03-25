9f1b2f01f4b9b14b077330bf32921738f557bc4f
GLinBoy <hojjatabedie@gmail.com>
2022-03-25 17:36:11 +0430

    📦 NEW: Added unit test for CityServiceImpl

---
+++ src/test/kotlin/com/glinboy/cities/service/impl/CityServiceImplTest.kt
@@ -0,0 +1,16 @@
+package com.glinboy.cities.service.impl
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CityServiceImplTest {
+
+    @Test
+    fun getAllCities() {
+    }
+
+    @Test
+    fun searchCity() {
+    }
+}
