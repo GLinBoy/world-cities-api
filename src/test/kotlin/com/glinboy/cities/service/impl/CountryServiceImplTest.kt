d9d94b9dab4e7f8aa71e28ec6cdf6cbb0a4c061b
GLinBoy <hojjatabedie@gmail.com>
2022-03-25 17:36:38 +0430

    📦 NEW: Added unit test for CountryServiceImpl

---
+++ src/test/kotlin/com/glinboy/cities/service/impl/CountryServiceImplTest.kt
@@ -0,0 +1,20 @@
+package com.glinboy.cities.service.impl
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CountryServiceImplTest {
+
+    @Test
+    fun getAllCountries() {
+    }
+
+    @Test
+    fun searchCountry() {
+    }
+
+    @Test
+    fun getCountryCities() {
+    }
+}
