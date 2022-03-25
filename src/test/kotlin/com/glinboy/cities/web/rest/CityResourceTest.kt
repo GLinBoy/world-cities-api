70b737361af01797157c22e02f645e2f4b32afcb
GLinBoy <hojjatabedie@gmail.com>
2022-03-25 17:36:57 +0430

    📦 NEW: Added unit test for CityResource

---
+++ src/test/kotlin/com/glinboy/cities/web/rest/CityResourceTest.kt
@@ -0,0 +1,16 @@
+package com.glinboy.cities.web.rest
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CityResourceTest {
+
+    @Test
+    fun getCities() {
+    }
+
+    @Test
+    fun searchCity() {
+    }
+}
