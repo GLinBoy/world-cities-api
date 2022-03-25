e721de9201121dfb42724d4f6e65bc5b2e7f4867
GLinBoy <hojjatabedie@gmail.com>
2022-03-25 17:37:11 +0430

    📦 NEW: Added unit test for CountryResource

---
+++ src/test/kotlin/com/glinboy/cities/web/rest/CountryResourceTest.kt
@@ -0,0 +1,20 @@
+package com.glinboy.cities.web.rest
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CountryResourceTest {
+
+    @Test
+    fun getCountries() {
+    }
+
+    @Test
+    fun searchCountry() {
+    }
+
+    @Test
+    fun getCountryCities() {
+    }
+}
