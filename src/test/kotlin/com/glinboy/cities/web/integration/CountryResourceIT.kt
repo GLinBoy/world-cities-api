fc8088516f1fb98812719811d954bdbca5a8c998
GLinBoy <hojjatabedie@gmail.com>
2022-04-05 18:41:04 +0200

    👌 IMPROVE: Added CountryResource integration test

---
+++ src/test/kotlin/com/glinboy/cities/web/integration/CountryResourceIT.kt
@@ -0,0 +1,20 @@
+package com.glinboy.cities.web.integration
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CountryResourceIT {
+
+    @Test
+    fun getCountries() {
+    }
+
+    @Test
+    fun searchCountry() {
+    }
+
+    @Test
+    fun getCountryCities() {
+    }
+}
