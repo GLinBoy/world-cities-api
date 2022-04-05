d8f463b3b4f1f71a01742ce8940d46f124dcdad5
GLinBoy <hojjatabedie@gmail.com>
2022-04-05 18:40:11 +0200

    👌 IMPROVE: Added CityResource integration test

---
+++ src/test/kotlin/com/glinboy/cities/web/integration/CityResourceIT.kt
@@ -0,0 +1,16 @@
+package com.glinboy.cities.web.integration
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CityResourceIT {
+
+    @Test
+    fun getCities() {
+    }
+
+    @Test
+    fun searchCity() {
+    }
+}
