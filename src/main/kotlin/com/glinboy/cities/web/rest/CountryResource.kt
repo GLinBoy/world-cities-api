7e89045960c4ac5a1a638a9cde947ea3dc9fb129
GLinBoy <hojjatabedie@gmail.com>
2023-05-23 18:37:54 +0200

    👌 IMPROVE: Replaced javax.* with jakarta.* in CountryResource
    #77

--- src/main/kotlin/com/glinboy/cities/web/rest/CountryResource.kt
+++ src/main/kotlin/com/glinboy/cities/web/rest/CountryResource.kt
@@ -4,11 +4,11 @@ import com.glinboy.cities.entity.City
 import com.glinboy.cities.projection.Country
 import com.glinboy.cities.service.CountryService
 import com.glinboy.cities.util.PaginationUtil
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Pattern
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import javax.validation.Valid
-import javax.validation.constraints.Pattern
 
 @RestController
 @RequestMapping("/api/v1/countries")
