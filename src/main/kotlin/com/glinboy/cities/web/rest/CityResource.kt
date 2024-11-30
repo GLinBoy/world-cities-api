29e6b71519668124b16c909261aaf4c9e0b09066
GLinBoy <glinboy@live.com>
2024-11-30 19:36:48 +0100

    👌 IMPROVE: Hide all Pageable inputs and enable them by @PageableAsQueryParam
    resolved #109

--- src/main/kotlin/com/glinboy/cities/web/rest/CityResource.kt
+++ src/main/kotlin/com/glinboy/cities/web/rest/CityResource.kt
@@ -3,8 +3,10 @@ package com.glinboy.cities.web.rest
 import com.glinboy.cities.entity.City
 import com.glinboy.cities.service.CityService
 import com.glinboy.cities.util.PaginationUtil
+import io.swagger.v3.oas.annotations.Parameter
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Pattern
+import org.springdoc.core.converters.models.PageableAsQueryParam
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,7 +22,8 @@ class CityResource(val cityService: CityService) {
     @JvmField final val BASE_CITY_SEARCH_URL = "/api/v1/cities/search?q=%s"
 
     @GetMapping
-    fun getCities(pageable: Pageable): ResponseEntity<List<City>> {
+    @PageableAsQueryParam
+    fun getCities(@Parameter(hidden = true) pageable: Pageable): ResponseEntity<List<City>> {
         val page = cityService.getAllCities(pageable)
         return ResponseEntity.ok()
             .headers(PaginationUtil.generateHeaders(page, BASE_CITY_URL))
@@ -28,9 +31,10 @@ class CityResource(val cityService: CityService) {
     }
 
     @GetMapping("search")
+    @PageableAsQueryParam
     fun searchCity(@Valid @Pattern(regexp = "\\p{L}+")
                    @RequestParam(name = "q", required = true) query: String,
-                   pageable: Pageable): ResponseEntity<List<City>> {
+                   @Parameter(hidden = true) pageable: Pageable): ResponseEntity<List<City>> {
         val page = cityService.searchCity(query, pageable)
         return ResponseEntity.ok()
             .headers(PaginationUtil.generateHeaders(page, BASE_CITY_SEARCH_URL.format(query)))
--- src/main/kotlin/com/glinboy/cities/web/rest/CountryResource.kt
+++ src/main/kotlin/com/glinboy/cities/web/rest/CountryResource.kt
@@ -4,8 +4,10 @@ import com.glinboy.cities.entity.City
 import com.glinboy.cities.projection.Country
 import com.glinboy.cities.service.CountryService
 import com.glinboy.cities.util.PaginationUtil
+import io.swagger.v3.oas.annotations.Parameter
 import jakarta.validation.Valid
 import jakarta.validation.constraints.Pattern
+import org.springdoc.core.converters.models.PageableAsQueryParam
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -19,7 +21,8 @@ class CountryResource(val countryService: CountryService) {
     @JvmField final val BASE_COUNTRY_CITIES_ISO2_URL = "/api/v1/countries/%s/cities"
 
     @GetMapping
-    fun getCountries(pageable: Pageable): ResponseEntity<List<Country>> {
+    @PageableAsQueryParam
+    fun getCountries(@Parameter(hidden = true) pageable: Pageable): ResponseEntity<List<Country>> {
         var page = countryService.getAllCountries(pageable)
         return ResponseEntity.ok()
             .headers(PaginationUtil.generateHeaders(page, BASE_URL))
@@ -27,9 +30,10 @@ class CountryResource(val countryService: CountryService) {
     }
 
     @GetMapping("search")
+    @PageableAsQueryParam
     fun searchCountry(@Valid @Pattern(regexp = "\\p{L}+")
                       @RequestParam(name = "q", required = true) query: String,
-                      pageable: Pageable): ResponseEntity<List<Country>> {
+                      @Parameter(hidden = true) pageable: Pageable): ResponseEntity<List<Country>> {
         val page = countryService.searchCountry(query, pageable)
         return ResponseEntity.ok()
             .headers(PaginationUtil.generateHeaders(page, BASE_COUNTRY_SEARCH_URL.format(query)))
@@ -37,7 +41,8 @@ class CountryResource(val countryService: CountryService) {
     }
 
     @GetMapping("{isoCode:[a-zA-Z]{2,3}}/cities")
-    fun getCountryCities(@PathVariable isoCode: String, pageable: Pageable): ResponseEntity<List<City>> {
+    @PageableAsQueryParam
+    fun getCountryCities(@PathVariable isoCode: String, @Parameter(hidden = true) pageable: Pageable): ResponseEntity<List<City>> {
         val page = countryService.getCountryCities(isoCode, pageable)
         return ResponseEntity.ok()
             .headers(PaginationUtil.generateHeaders(page, BASE_COUNTRY_CITIES_ISO2_URL.format(isoCode)))
